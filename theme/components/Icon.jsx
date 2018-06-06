9c912332a3f16d300eda8ef0c32f088c06874cbe
suxiaoxin <hellosean1025@163.com>
2018-06-06 11:14:13 +0800

    feat: init

---
+++ theme/components/Icon.jsx
@@ -0,0 +1,3 @@
+<link  xx="y" rel="apple-touch-icon" sizes="180x180" href={relePath(props.distPath, 'ydoc/images/apple-touch-icon.png')} />
+<link  xx="y" rel="icon" type="image/png" sizes="32x32" href={relePath(props.distPath, 'ydoc/images/favicon-32x32.png')} />
+<link  xx="y" rel="icon" type="image/png" sizes="16x16" href={relePath(props.distPath, 'ydoc/images/favicon-16x16.png')} />
